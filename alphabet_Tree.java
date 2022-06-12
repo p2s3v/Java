b1eaae38b059b5bcec63b16bb9021e914bb143bd
Vikas Patil <vikipatil8991@gmail.com>
2022-06-12 15:49:14 +0530

    Alphabet tree

---
+++ alphabet_Tree.java
@@ -0,0 +1,19 @@
+
+public class alphabet_Tree {
+
+	public static void main(String []args)
+	{
+		char ch=65;// byte code value for A
+		
+		for(int i=1;i<=4;i++)
+		{ ch=65;
+			for( int j=1;j<=i;j++)
+			{
+				System.out.print(ch+" ");
+				ch++;
+			}
+			System.out.println();
+			
+		}
+	}
+}
